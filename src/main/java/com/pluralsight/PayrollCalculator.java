7d57565014aa5c1b8795b3e589ec56a4ec45c943
nati-tewolde <natisnewemail@gmail.com>
2025-09-25 11:32:06 -0400

    Completed Mod4 - Exercise 4 - Payroll Calculator

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollCalculator {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your name: ");
+        String name = input.nextLine();
+
+        System.out.print("How many hours have you worked? ");
+        float hoursWorked = input.nextFloat();
+
+        System.out.print("What is your hourly pay rate? ");
+        float payRate = input.nextFloat();
+
+        float grossPay;
+        if (hoursWorked > 40) {
+            grossPay = (40 * payRate) + (hoursWorked - 40) * (payRate * 1.5f);
+        } else {
+            grossPay = hoursWorked * payRate;
+        }
+        System.out.println("Employee " + name + " has made a total of $" + grossPay);
+
+    }
 }
